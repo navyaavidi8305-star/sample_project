2ee8d84f98d0666ceea99bec14e9f79436eedda0
navyaavidi8305-star <navyaavidi8305@gmail.com>
2025-08-02 12:20:36 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask 
+from flask import Flask,render_template 
 app=Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Welcome to MRECW"
+    return render_template("index.html")
 @app.route("/home")
 def home():
     return "home page"
